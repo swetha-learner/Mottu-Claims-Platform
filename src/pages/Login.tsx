4c49976f97e8b64d5ccae3cb86ab622acf78e7ee
swetha udayakumar <swethaudayakumar3990@gmail.com>
2026-06-30 11:24:49 +0530

    initial setup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,12 @@
 function Login() {
     return (
-        <h1> Login to your account!</h1>
+        <div>
+            <h1> Login page</h1>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input className="w-50 h-5 p-5 placeholder:text-red-300 ring-2 ring-blue-500 rounded-full" type="text" id="username" name="username" placeholder="Enter your username"/>
+            </div>
+        </div>
     )
 }
 
